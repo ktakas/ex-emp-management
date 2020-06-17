54b0a45db90f15db00f17d8a95b0126b3c3a97b3
ktakas <66981906+ktakas@users.noreply.github.com>
2020-06-17 12:15:32 +0900

    管理者情報を登録する機能の作成
    AdministratorServiceにinsertメソッドを追加
    
    AdministratorControllerにinsertメソッドを追加

--- src/main/java/jp/co/sample/service/AdministratorService.java
+++ src/main/java/jp/co/sample/service/AdministratorService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import jp.co.sample.domain.Administrator;
 import jp.co.sample.repository.AdministratorRepository;
 
 /**
@@ -18,5 +19,14 @@ public class AdministratorService {
 
 	@Autowired
 	private AdministratorRepository administratorRepository;
+	
+	/**
+	 * Administratorリポジトリのinsertメソッドを呼び出し管理者情報を挿入する
+	 * 
+	 * @param administrator
+	 */
+	public void insert(Administrator administrator) {
+		administratorRepository.insert(administrator);
+	}
 	
 }
