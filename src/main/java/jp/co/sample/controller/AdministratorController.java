b4601c04e95eb045463aec6439bc9d423cfc0e69
ktakas <66981906+ktakas@users.noreply.github.com>
2020-06-17 12:27:45 +0900

    ログイン画面を表示する機能の作成
    LoginFormクラスの定義
    
    AdministratorControllerクラスにメソッドを追加
    
    login.htmlの作成

--- src/main/java/jp/co/sample/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import jp.co.sample.domain.Administrator;
 import jp.co.sample.form.InsertAdministratorForm;
+import jp.co.sample.form.LoginForm;
 import jp.co.sample.service.AdministratorService;
 
 /**
@@ -24,6 +25,16 @@ public class AdministratorController {
 	private AdministratorService administratorService;
 	
 	/**
+	 * LoginFormをインスタンス化する
+	 * 
+	 * @return LoginForm
+	 */
+	@ModelAttribute
+	public LoginForm setUpLoginForm() {
+		return new LoginForm();
+	}
+	
+	/**
 	 * InsertAdministratorFormをインスタンス化する
 	 * 
 	 * @return InsertAdministratorForm
@@ -34,15 +45,31 @@ public class AdministratorController {
 	}
 	
 	/**
+	 * administrator/login.htmlにフォワードする
+	 * 
+	 * @return 管理者ログイン画面へのパス
+	 */
+	@RequestMapping("/")
+	public String toLogin() {
+		return "administrator/login";
+	}
+	
+	/**
 	 * administrator/insert.htmlにフォワードする
 	 * 
-	 * @return String
+	 * @return 管理者情報を登録する処理へのパス
 	 */
 	@RequestMapping("/toInsert")
 	public String toInsert() {
 		return "administrator/insert";
 	}
 	
+	/**
+	 * 引数のフォームからAdministratorのインスタンスへ情報をコピーし、サービスへ渡す
+	 * 
+	 * @param form
+	 * @return ログイン画面へのパス
+	 */
 	@RequestMapping("/insert")
 	public String insert(InsertAdministratorForm form) {
 		Administrator administrator = new Administrator();
